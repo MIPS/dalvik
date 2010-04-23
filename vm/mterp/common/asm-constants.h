7fdfeb80e13497d3bdc75a2b5c3be4b49c87bc61
Chris Dearman <chris@mips.com>
2010-04-23 10:25:55 -0700

    [ENDIAN] Pad the 8 and 16 bit Jvalue members
    This allows them to be consistently accessed as either 8-bit,
    16-bit or 32-bit values
    
    Current failures:
    failed: 003-omnibus-opcodes
    failed: 030-bad-finalizer		[passes with differnt status message]
    failed: 051-thread			[faulty test?]
    failed: 057-iteration-performance	[emulation speed]
    failed: 062-character-encodings		[expected]
    failed: 071-dexfile			[expected]

--- vm/mterp/common/asm-constants.h
+++ vm/mterp/common/asm-constants.h
@@ -135,7 +135,11 @@ MTERP_OFFSET(offGlue_jitResumePC,       MterpGlue, jitResumePC, 60)
 #endif
 #endif
 /* make sure all JValue union members are stored at the same offset */
+#ifdef HAVE_BIG_ENDIAN
+MTERP_OFFSET(offGlue_retval_z,          MterpGlue, retval.z, 11)
+#else
 MTERP_OFFSET(offGlue_retval_z,          MterpGlue, retval.z, 8)
+#endif
 MTERP_OFFSET(offGlue_retval_i,          MterpGlue, retval.i, 8)
 MTERP_OFFSET(offGlue_retval_j,          MterpGlue, retval.j, 8)
 MTERP_OFFSET(offGlue_retval_l,          MterpGlue, retval.l, 8)
