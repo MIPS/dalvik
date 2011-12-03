f7c0b73dc2d74ee78b47a4c2c71a4b643f144ef2
Chris Dearman <chris@mips.com>
2011-12-02 19:32:28 -0800

    Use old frame for dvmReportPreNativeInvoke/dvmReportPostNativeInvoke
    The portable and standard interpretes were using different values.
    The portable interpeter is correct...
    
    Signed-off-by: Chris Dearman <chris@mips.com>

--- vm/mterp/out/InterpC-portable.cpp
+++ vm/mterp/out/InterpC-portable.cpp
@@ -5340,7 +5340,7 @@ GOTO_TARGET(invokeMethod, bool methodCallRange, const Method* _methodToCall,
             DUMP_REGS(methodToCall, newFp, true);   // show input args
 
             if (self->interpBreak.ctl.subMode != 0) {
-                dvmReportPreNativeInvoke(methodToCall, self, fp);
+                dvmReportPreNativeInvoke(methodToCall, self, newSaveArea->prevFrame);
             }
 
             ILOGD("> native <-- %s.%s %s", methodToCall->clazz->descriptor,
@@ -5354,7 +5354,7 @@ GOTO_TARGET(invokeMethod, bool methodCallRange, const Method* _methodToCall,
             (*methodToCall->nativeFunc)(newFp, &retval, methodToCall, self);
 
             if (self->interpBreak.ctl.subMode != 0) {
-                dvmReportPostNativeInvoke(methodToCall, self, fp);
+                dvmReportPostNativeInvoke(methodToCall, self, newSaveArea->prevFrame);
             }
 
             /* pop frame off */
