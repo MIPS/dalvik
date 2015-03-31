0ba8b9bab732545721e34f76c3ca1dba2db6cca6
Miroslav Tisma <miroslav.tisma@imgtec.com>
2015-03-31 17:49:55 +0200

    [MIPS] Fix a problem with breakpoint handling
    This patch fixes the problem with wrong handling of OP_BREAKPOINT
    when using java debugger.
    
    Original bug report:
    https://code.google.com/p/android/issues/detail?id=81861
    
    First issue was that C version of OP_BREAKPOINT handler wasn't
    completely implemented. It was missing FINISH_BKPT macro implementation.
    The OP_BREAKPOINT handler was switched to platform specific version,
    which also needed an update to use v0 register instead of a0.
    After this changes java debugger works as expected.
    
    Change-Id: Iafbb08ffa136a9587496550ed0eb16cafb0153fa

--- vm/mterp/out/InterpC-mips.cpp
+++ vm/mterp/out/InterpC-mips.cpp
@@ -1162,32 +1162,6 @@ GOTO_TARGET_DECL(exceptionThrown);
     }                                                                       \
     FINISH(2);
 
-/* File: c/OP_BREAKPOINT.cpp */
-HANDLE_OPCODE(OP_BREAKPOINT)
-    {
-        /*
-         * Restart this instruction with the original opcode.  We do
-         * this by simply jumping to the handler.
-         *
-         * It's probably not necessary to update "inst", but we do it
-         * for the sake of anything that needs to do disambiguation in a
-         * common handler with INST_INST.
-         *
-         * The breakpoint itself is handled over in updateDebugger(),
-         * because we need to detect other events (method entry, single
-         * step) and report them in the same event packet, and we're not
-         * yet handling those through breakpoint instructions.  By the
-         * time we get here, the breakpoint has already been handled and
-         * the thread resumed.
-         */
-        u1 originalOpcode = dvmGetOriginalOpcode(pc);
-        ALOGV("+++ break 0x%02x (0x%04x -> 0x%04x)", originalOpcode, inst,
-            INST_REPLACE_OP(inst, originalOpcode));
-        inst = INST_REPLACE_OP(inst, originalOpcode);
-        FINISH_BKPT(originalOpcode);
-    }
-OP_END
-
 /* File: c/gotoTargets.cpp */
 /*
  * C footer.  This has some common code shared by the various targets.
